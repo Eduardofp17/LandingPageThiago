be0a22c9a72429aa2164d9bf60f51a3e55eaa76d
Eduardofp <edardofernandespinheiro@gmail.com>
2023-01-08 15:43:40 -0300

    Teste responsividade e performance

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,17 +15,17 @@ const shapeTopC6 = document.querySelector("#shapeTopC6");
 const ctxShapeTopC6 = shapeTopC6.getContext("2d");
 
 const bottomC3IMG = new Image();
-bottomC3IMG.src = "/assets/images/teste.png";
+bottomC3IMG.src = "/assets/images/teste.webp";
 bottomC3IMG.onload = () => ctx.drawImage(bottomC3IMG, -100, 35); 
 
 const shapeC1IMG = new Image();
-shapeC1IMG.src = "/assets/images/shapeBottom.png";
+shapeC1IMG.src = "/assets/images/shapeBottom.webp";
 shapeC1IMG.onload = () => ctxShapeC1.drawImage(shapeC1IMG, 0, -1); 
 
 const shapeBC6IMG = new Image();
-shapeBC6IMG.src = "/assets/images/shapeBottomC6.png";
+shapeBC6IMG.src = "/assets/images/shapeBottomC6.webp";
 shapeBC6IMG.onload = () => ctxShapeBottomC6.drawImage(shapeBC6IMG, 0, 59); 
 
 const shapeTC6IMG = new Image();
-shapeTC6IMG.src = "/assets/images/shapeTopC6.png";
+shapeTC6IMG.src = "/assets/images/shapeTopC6.webp";
 shapeTC6IMG.onload = () => ctxShapeTopC6.drawImage(shapeTC6IMG, 0, 0); 
